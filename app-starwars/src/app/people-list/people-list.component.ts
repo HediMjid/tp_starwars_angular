e143d3a6b3a85da4f6b31d34fca989dbd6ebe9dd
HediMjid <hedi.mjid@gmail.com>
2021-11-29 16:48:24 +0100

    fix rleoad component

--- app-starwars/src/app/people-list/people-list.component.ts
+++ app-starwars/src/app/people-list/people-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceSwapiService } from '../service-swapi.service';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-people-list',
@@ -10,12 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 export class PeopleListComponent implements OnInit {
   peoples : any;
   page: any;
-  constructor(private httpService: ServiceSwapiService, private route: ActivatedRoute) { }
+  constructor(private httpService: ServiceSwapiService, private route: ActivatedRoute, private router: Router) {
+    this.router.routeReuseStrategy.shouldReuseRoute = function(){
+      return false;
+    };
+   }
 
   ngOnInit(): void {
     const page = Number(this.route.snapshot.paramMap.get('page'));
+  
     this.httpService.getPeoples(page).subscribe(
-      (response) => { this.peoples = response; console.log(this.peoples); this.ngOnInit();}
+      (response) => { this.peoples = response; console.log(this.peoples);}
     );
   }
 
